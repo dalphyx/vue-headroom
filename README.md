f1dbcaa472bb379f11cf71673514ba86b29219df
dalphyx <wjcbmk@gmail.com>
2016-09-02 18:26:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# vue-headroom [![npm package](https://img.shields.io/npm/v/vue-headroom.svg)](https://www.npmjs.com/package/vue-headroom)
+
+> Headroom for Vuejs.
+
+# Requirements
+
+- [Vue.js](https://github.com/vuejs/vue) `^2.0.0`
+- [webpack](https://github.com/webpack/webpack) `^2.0.0`
+
+# Installation
+
+``` bash
+$ npm install vue-headroom
+```
+
+# Usage
+``` html
+<template>
+  <headroom>
+    <div>
+      // Put your head code here...
+    </div>
+  </headroom>
+</template>
+
+<script>
+import { headroom } from 'vue-headroom'
+
+export default {
+  components: {
+    headroom
+  }
+}
+</script>
+```
+
+# Props
+
+`speed`
+
+Transition speed, in ms. Default: `350`
+
+`easing`
+
+Transition function. Default: `ease-in-out`
+
+`disabled`
+
+Disable the headroom. Default: false
+
+`upTolerance`
+
+Scroll tolerance when scrolling up before component is pinned, in px. Default: `5`
+
+`downTolerance`
+
+Height of progress bar. Default: `3`
+
+`onPin`
+
+Callback when header is pinned.
+
+`onUnpin`
+
+Callback when header is unpinned.
+
+`pinStart`
+
+Height in px where the header should start and stop pinning. Default: `0`
+
+`zIndex`
+
+The z-index of component. Default: `9999`
+
+# License
+
+[The MIT License](http://opensource.org/licenses/MIT)
---
+++ src/checkActions.js
@@ -0,0 +1,56 @@
+function checkActions (states) {
+  const direction = states.currentScrollY >= states.lastScrollY ? 'down' : 'up'
+  const distanceScrolled = Math.abs(states.currentScrollY - states.lastScrollY)
+
+  let action
+
+  // Begining, at the top and not fixed.
+  if (
+    states.currentScrollY <= states.pinStart &&
+    states.state !== 'unfixed'
+  ) {
+    action = 'unfix'
+
+  // Still unfixed, scrolling down and passing the header.
+  } else if (
+    states.currentScrollY <= states.elemHeight &&
+    direction === 'down' &&
+    states.state === 'unfixed'
+  ) {
+    action = 'none'
+
+  // Scrolling down and past the header.
+  // Unpinned the header.
+  } else if (
+    direction === 'down' &&
+    ['pinned', 'unfixed'].indexOf(states.state) >= 0 &&
+    states.currentScrollY > (states.elemHeight + states.pinStart) &&
+    distanceScrolled > states.downTolerance
+  ) {
+    action = 'unpin'
+
+  // Now, it's time to up.
+  // Pin the header.
+  } else if (
+    direction === 'up' &&
+    distanceScrolled > states.upTolerance &&
+    ['pinned', 'unfixed'].indexOf(states.state) < 0
+  ) {
+    action = 'pin'
+
+  // Still scrolling up and passing the header.
+  // Pin the header regardless of upTolerance.
+  } else if (
+    direction === 'up' &&
+    states.currentScrollY <= states.elemHeight &&
+    ['pinned', 'unfixed'].indexOf(states.state) < 0
+  ) {
+    action = 'pin'
+  } else {
+    action = 'none'
+  }
+
+  return action
+}
+
+export default checkActions
