a2cd7b9b2c216a59c55db199e6f21d601db4cf36
dalphyx <wjcbmk@gmail.com>
2017-08-07 15:29:47 +0800

    make vue-headroom as plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import headroom from './headroom.vue'
 
-export default headroom
+const install = function (Vue, opt = {}) {
+  if (install.installed) {
+    return
+  }
+
+  Vue.component('vue-headroom', headroom)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  headroom,
+  install
+}
