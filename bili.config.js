10c16f75547769cc3b9ed79862c466cbdc22a5b7
dalphyx <wjcbmk@gmail.com>
2018-09-29 17:46:47 +0800

    use bili

---
+++ bili.config.js
@@ -0,0 +1,30 @@
+const babel = require('rollup-plugin-babel')
+const vue = require('rollup-plugin-vue').default
+
+module.exports = {
+  name: 'vue-headroom',
+  moduleName: 'vueHeadroom',
+  input: 'src/index.js',
+  format: ['cjs', 'umd', 'umd-min', 'es', 'iife'],
+  plugins: [
+    vue({
+      compileTemplate: true
+    }),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          'es2015',
+          {
+            'modules': false
+          }
+        ],
+        'stage-2'
+      ]
+    })
+  ],
+  banner: {
+    year: 2016
+  }
+}
